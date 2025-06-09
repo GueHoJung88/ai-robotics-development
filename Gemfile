34e132c1d07b4c6bf65e47368ea1457acc243c1e
GueHoJung88 <emotionalmachine88@gmail.com>
2025-06-10 06:00:47 +0900

    apply tutorial

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+# Minimal Mistakes 테마 및 필수 플러그인
+
+gem "github-pages", group: :jekyll_plugins
+
+gem "jekyll-include-cache"
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,10 @@
+var store = [{
+        "title": "FISRT-TITLE",
+        "excerpt":"First Post For Github Pages.      Let’s Do It!!!   ","categories": [],
+        "tags": [],
+        "url": "/2025/06/07/first-post.html",
+        "teaser": null
+      },{
+    "title": "AI 로보틱스 개발자의 여정",
+    "excerpt":"AI 로보틱스 개발자의 여정   이 블로그는 인공지능 로보틱스 전문 개발자가 되어가는 여정을 기록하는 공간입니다.  최신 AI, 로봇, 개발 기술과 경험을 공유하며, 성장하는 과정을 담아갑니다.  ","url": "http://localhost:4000/"
+  }]
